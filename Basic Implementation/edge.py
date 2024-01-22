4c8aa3987765a67b53c0ac4ca24327e059db2fb6
Raza <141234596+KMORaza@users.noreply.github.com>
2024-01-22 12:57:24 +0530

    -

--- Basic Implementation/edge.py
+++ Basic Implementation/edge.py
@@ -5,7 +5,6 @@ class Edge:
         self.end_node = end_node
         self.calculate_distance()
     def calculate_distance(self):
-        # Simple Euclidean distance as the edge weight
         dx = self.start_node.x - self.end_node.x
         dy = self.start_node.y - self.end_node.y
         self.distance = (dx**2 + dy**2)**0.5
